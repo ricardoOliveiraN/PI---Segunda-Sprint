9016fae0f0b549687f5d194f9a2d97898954b941
oizaeljr <izael.oliveirajr@sptech.school>
2024-09-21 00:13:16 -0300

    Adicionei a interação da mudança de tela

---
+++ funcionalidades.js
@@ -0,0 +1,11 @@
+function AbrirLogin() {
+    const urlTela = 'login.html';
+    window.close();
+    window.open(urlTela);
+}
+
+function AbrirCadastro() {
+    const urlTela = 'cadastro.html';
+    window.close();
+    window.open(urlTela);
+}
