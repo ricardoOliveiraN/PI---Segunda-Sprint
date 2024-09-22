a0ba8be194108923e2ec415bcf1c1118c507af8f
oizaeljr <izael.oliveirajr@sptech.school>
2024-09-22 15:40:09 -0300

    Coloquei a função de visualizar senha

--- funcionalidades.js
+++ funcionalidades.js
@@ -9,3 +9,13 @@ function AbrirCadastro() {
     window.close();
     window.open(urlTela);
 }
+
+function VisualizarSenha() {
+    if (txt_senha2.type === 'password') {
+        txt_senha2.type = 'text';
+        img_olho3.src = 'Imagens/olho_fechado.png';
+    } else {
+        txt_senha2.type = 'password';
+        img_olho3.src = 'Imagens/olho_aberto.png';
+    }
+}
