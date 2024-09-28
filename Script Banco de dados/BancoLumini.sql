bdab0fafbfebaf406beb09a133c853736cdebc1e
fabiamdamaceno <fabiam.silva@sptech.school>
2024-09-28 12:33:05 -0300

    Adicionado constraints na tabela empresa

--- Script Banco de dados/BancoLumini.sql
+++ Script Banco de dados/BancoLumini.sql
@@ -8,14 +8,24 @@ CREATE TABLE empresa (
     nomeFantasia VARCHAR(45),
     cnpj VARCHAR(18),
     tamanhoEmpresa VARCHAR(45),
+		CONSTRAINT chkTamanhoEmpresa
+		CHECK (tamanhoEmpresa IN('Pequena', 'Média', 'Grande')), -- receita operacional em milhão -> pequena: 0,36 - 4,8; média: 4,8 - 300; grande: 300 e além
     dtSaida DATE,
+		CONSTRAINT chkDtSaida
+        CHECK (dtSaida > dtCriacao),
     cep VARCHAR(9),
     uf CHAR(2),
+		CONSTRAINT chckEstado 
+        CHECK (estado IN(
+        'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 
+        'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SE', 'SP', 'TO')),
     cidade VARCHAR(45),
     logradouro VARCHAR(45),
     complemento VARCHAR(45),
     qtdHectares INT,
     statusCadastro VARCHAR(7),
+		CONSTRAINT chkEstado
+        CHECK (statusCadastro IN('ativo', 'inativo')),
     dataSaida DATE
 );
 
