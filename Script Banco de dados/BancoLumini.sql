ee4447cc006f17dbbbc424dd521dc641b82e1b8c
fabiamdamaceno <fabiam.silva@sptech.school>
2024-09-28 13:00:06 -0300

    Adição de constraints na tabela usuario, configuração da foreign nessa tabela; Reescrita na modelagem e no script das fks para aderir à convenção de escrita

--- Script Banco de dados/BancoLumini.sql
+++ Script Banco de dados/BancoLumini.sql
@@ -4,15 +4,12 @@ USE lumini;
 
 CREATE TABLE empresa (
 	idEmpresa INT,
-    dtCriacao DATE,
     nomeFantasia VARCHAR(45),
     cnpj VARCHAR(18),
     tamanhoEmpresa VARCHAR(45),
 		CONSTRAINT chkTamanhoEmpresa
 		CHECK (tamanhoEmpresa IN('Pequena', 'Média', 'Grande')), -- receita operacional em milhão -> pequena: 0,36 - 4,8; média: 4,8 - 300; grande: 300 e além
-    dtSaida DATE,
-		CONSTRAINT chkDtSaida
-        CHECK (dtSaida > dtCriacao),
+	qtdHectares INT,
     cep VARCHAR(9),
     uf CHAR(2),
 		CONSTRAINT chckEstado 
@@ -22,29 +19,35 @@ CREATE TABLE empresa (
     cidade VARCHAR(45),
     logradouro VARCHAR(45),
     complemento VARCHAR(45),
-    qtdHectares INT,
     statusCadastro VARCHAR(7),
 		CONSTRAINT chkEstado
         CHECK (statusCadastro IN('ativo', 'inativo')),
-    dataSaida DATE
+	dtCriacao DATE,
+	dtSaida DATE,
+		CONSTRAINT chkDtSaida
+        CHECK (dtSaida > dtCriacao) -- data de criação deve ser obrigatoriamente menor que a data de inativação
 );
 
 CREATE TABLE usuario (
 	idUsuario INT,
-    dtCriacao DATE,
     nome VARCHAR(45),
     senha VARCHAR (45),
     email VARCHAR(45),
     telefone VARCHAR(16),
     tipoUsuario VARCHAR(9),
-    fkEmpresa_Usuario INT
+		CONSTRAINT chkTipoUsuario
+		CHECK (tipoUsuario IN('Responsável', 'Comum', 'Convidado')),
+	dtCriacao DATE,
+    fkUsuario_Empresa INT,
+		CONSTRAINT fkEmpresa FOREIGN KEY (fkUsuario_Empresa)
+		REFERENCES empresa(idEmpresa)
 );
 
 CREATE TABLE talhao (
 	idTalhao INT,
     numero INT,
     areaTalhao INT,
-    fkEmpresa_Talhao INT
+    fkTalhao_Empresa INT
 );
 
 CREATE TABLE sensor (
@@ -53,8 +56,8 @@ CREATE TABLE sensor (
     statusFuncionamento VARCHAR(11),
     dtInstalacao DATE,
     dtUltimaManutencao DATE,
-    fkEmpresa_Sensor INT,
-    fkTalhao_Sensor INT
+    fkSensor_Empresa INT,
+    fkSensor_Talhao INT
 );
 
 CREATE TABLE dadosSensor (
@@ -62,5 +65,5 @@ CREATE TABLE dadosSensor (
     qtdLuz INT,
     momentoCaptura DATETIME,
     statusLuminosidade VARCHAR(12),
-    fkSensor_DadosSensor INT
+    fkDadosSensor_Sensor INT
 );
