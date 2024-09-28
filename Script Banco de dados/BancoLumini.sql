00d474ad218f79bb464638ab1acb545d5d53a282
fabiamdamaceno <fabiam.silva@sptech.school>
2024-09-28 13:42:39 -0300

    No BD, adicionado constraints e configuração das fks na tabela sensor

--- Script Banco de dados/BancoLumini.sql
+++ Script Banco de dados/BancoLumini.sql
@@ -25,7 +25,7 @@ CREATE TABLE empresa (
 	dtCriacao DATE,
 	dtSaida DATE,
 		CONSTRAINT chkDtSaida
-        CHECK (dtSaida > dtCriacao) -- data de criação deve ser obrigatoriamente menor que a data de inativação
+        CHECK (dtSaida > dtCriacao) -- data de criação deve ser obrigatoriamente antes que a data de inativação
 );
 
 CREATE TABLE usuario (
@@ -39,7 +39,7 @@ CREATE TABLE usuario (
 		CHECK (tipoUsuario IN('Responsável', 'Comum', 'Convidado')),
 	dtCriacao DATE,
     fkUsuario_Empresa INT,
-		CONSTRAINT fkReEmpresa FOREIGN KEY (fkUsuario_Empresa)
+		CONSTRAINT fkReUsuario_Empresa FOREIGN KEY (fkUsuario_Empresa)
 		REFERENCES empresa(idEmpresa)
 );
 
@@ -48,17 +48,25 @@ CREATE TABLE talhao (
     numero INT,
     areaTalhao INT,
     fkTalhao_Empresa INT,
-		CONSTRAINT fkReEmpresa FOREIGN KEY (fkTalhao_Empresa)
+		CONSTRAINT fkReTalhao_Empresa FOREIGN KEY (fkTalhao_Empresa)
 		REFERENCES empresa(idEmpresa)
 );
 
 CREATE TABLE sensor (
 	idSensor INT,
-    statusFuncionamento VARCHAR(11),
+    statusFuncionamento VARCHAR(10),
+		CONSTRAINT chkStatusFuncionamento
+		CHECK (statusFuncionamento IN('Ativo', 'Inativo', 'Manutenção')),
     dtInstalacao DATE,
     dtUltimaManutencao DATE,
+		CONSTRAINT chkUltimaManutencao
+        CHECK (dtUltimaManutencao >= dtInstalacao), -- data da última manuntenção deve ser obrigatoriamente depois da data de instalação
     fkSensor_Empresa INT,
-    fkSensor_Talhao INT
+		CONSTRAINT fkReSensor_Empresa FOREIGN KEY (fkSensor_Empresa)
+		REFERENCES empresa(idEmpresa),
+    fkSensor_Talhao INT,
+		CONSTRAINT fkReSensor_Talhao FOREIGN KEY (fkSensor_Talhao)
+		REFERENCES empresa(idTalhao)
 );
 
 CREATE TABLE dadosSensor (
