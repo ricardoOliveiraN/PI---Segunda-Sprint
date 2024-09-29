586c0fbd0310e9da21d281cd798dd3808e8535c6
fabiamdamaceno <fabiam.silva@sptech.school>
2024-09-29 15:08:10 -0300

    Em BD, adicionado campo fkEmpresa_EmpresaSede com autorelacionamento na tabela empresa

--- Script Banco de dados/BancoLumini.sql
+++ Script Banco de dados/BancoLumini.sql
@@ -27,8 +27,11 @@ CREATE TABLE empresa (
 	dtCriacao DATE,
 	dtSaida DATE,
 		CONSTRAINT chkDtSaida
-        CHECK (dtSaida > dtCriacao OR dtSaida = null) 
+        CHECK (dtSaida > dtCriacao OR dtSaida = null),
         -- data de criação deve ser obrigatoriamente antes que a data de inativação
+	fkEmpresa_EmpresaSede INT,
+		CONSTRAINT fkReEmpresa_EmpresaSede FOREIGN KEY (fkEmpresa_EmpresaSede)
+		REFERENCES empresa(idEmpresa)
 );
 
 CREATE TABLE usuario (
@@ -107,17 +110,19 @@ CREATE TABLE dadosSensor (
 		REFERENCES sensor(idSensor)
 );
 
-INSERT INTO empresa (nomeFantasia, cnpj, tamanhoEmpresa, qtdHectares, cep, uf, cidade, logradouro, complemento, statusCadastro, dtCriacao, dtSaida) VALUES
-	('Agrosil', '12.345.678/0001-90', 'Pequena', 15, '12345-678', 'SP', 'Vale do Ribeira', 'Rua das Flores', 'Casa 1', 'ativo', '2024-08-20', '2023-09-01'),
-	('LúFazendas', '23.456.789/0001-01', 'Média', 20, '23456-789', 'RS', 'Serra Gaúcha', 'Avenida da Paz', 'Lote 10', 'ativo', '2024-08-30', NULL),
-	('Lúpulo da Terra',  '45.678.901/0001-23', 'Grande', 500, '45678-901', 'SP', 'Campos do Jordão', 'Rua do Lúpulo', 'Chácara 2', 'ativo', '2024-09-05', NULL);
-
+INSERT INTO empresa (nomeFantasia, cnpj, tamanhoEmpresa, qtdHectares, cep, uf, cidade, logradouro, complemento, statusCadastro, dtCriacao, dtSaida, fkEmpresa_EmpresaSede) VALUES
+	('Agrosil', '12.345.678/0001-90', 'Pequena', 15, '12345-678', 'SP', 'Vale do Ribeira', 'Rua das Flores', 'Casa 1', 'ativo', '2024-08-20', '2023-09-01', NULL),
+	('LúFazendas', '23.456.789/0001-01', 'Média', 20, '23456-789', 'RS', 'Serra Gaúcha', 'Avenida da Paz', 'Lote 10', 'ativo', '2024-08-30', NULL, NULL),
+	('Lúpulo da Terra',  '45.678.901/0001-23', 'Grande', 500, '45678-901', 'SP', 'Campos do Jordão', 'Rua do Lúpulo', 'Chácara 2', 'ativo', '2024-09-05', NULL, NULL),
+	('Lúpulo da Terra Nordeste',  '54.876.109/0001-23', 'Média', 250, '45678-901', 'BA', 'Senhor do Bonfim', 'Rua da Esperança', 'Lote 2', 'ativo', '2024-09-15', NULL, 3);
+    
 INSERT INTO usuario (nome, senha, email, telefone, tipoUsuario, statusUsuario ,dtCriacao, dtExclusao, fkUsuario_Empresa) VALUES
 	('João Silva', MD5('Sol!123'), 'joao.silva@email.com', '11987654321', 'Responsável', 'inativo','2024-08-30','2023-09-01', 1),
-	('Maria Oliveira', MD5('Céu@456'), 'maria.oliveira@email.com', '21987654321', 'Comum', 'ativo','2024-08-30', null, 2),
-	('Carlos Souza', MD5('Floresta#789'), 'carlos.souza@email.com', '31987654321', 'Responsável', 'ativo','2024-08-30', null, 3),
-	('Ana Pereira', MD5('Mar$3Clamo'), 'ana.pereira@email.com', '41987654321', 'Comum', 'ativo','2024-08-30', null, 3),
-	('Ricardo Lima', MD5('Lua%8Cheia'), 'ricardo.lima@email.com', '51987654321', 'Convidado', 'inativo','2024-08-30', '2024-09-30', 3);
+	('Maria Oliveira', MD5('Céu@456'), 'maria.oliveira@email.com', '21987654321', 'Comum', 'ativo','2024-08-30', NULL, 2),
+	('Carlos Souza', MD5('Floresta#789'), 'carlos.souza@email.com', '31987654321', 'Responsável', 'ativo','2024-09-05', NULL, 3),
+	('Ana Pereira', MD5('Mar$3Clamo'), 'ana.pereira@email.com', '41987654321', 'Comum', 'ativo','2024-09-05', NULL, 3),
+	('Ricardo Lima', MD5('Lua%8Cheia'), 'ricardo.lima@email.com', '51987654321', 'Convidado', 'inativo','2024-09-10', '2024-10-10', 3),
+    ('Joane Damaceno', MD5('Vasc0#'), 'joane.damaceno@email.com', '21987123456', 'Responsável', 'ativo','2024-09-15', NULL, 4);
     
 INSERT INTO talhao (numero, areaTalhao, fkTalhao_Empresa) VALUES
 	(1, 5000, 1),
@@ -125,17 +130,19 @@ INSERT INTO talhao (numero, areaTalhao, fkTalhao_Empresa) VALUES
 	(2, 150000, 2),
 	(1, 60000, 3),
 	(2, 120000, 3),
-	(3, 300000, 3);
+	(3, 300000, 3),
+    (1, 160000, 4);
 
 INSERT INTO sensor (statusFuncionamento, dtInstalacao, dtUltimaManutencao, fkSensor_Empresa, fkSensor_Talhao) VALUES
 	('Inativo', '2024-09-05', '2024-09-20', 1, 100), 
-	('Inativo', '2024-09-05', null, 1, 100), 
-	('Manutenção', '2024-09-08', null, 2, 101), 
+	('Inativo', '2024-09-05', NULL, 1, 100), 
+	('Manutenção', '2024-09-08', NULL, 2, 101), 
 	('Ativo', '2024-09-10', '2024-09-12', 3,102),
-	('Inativo', '2024-09-20', null, 2, 103),
-	('Ativo', '2024-09-20', null, 3, 104), 
-	('Ativo', '2024-09-21', null, 2, 105),
-	('Ativo', '2024-09-30', null, 3, 105); 
+	('Inativo', '2024-09-20', NULL, 2, 103),
+	('Ativo', '2024-09-20', NULL, 3, 104), 
+	('Ativo', '2024-09-21', NULL, 2, 105),
+	('Ativo', '2024-09-30', NULL, 3, 105),
+	('Ativo', '2024-09-30', NULL, 4, 106);
 
 INSERT INTO dadosSensor (qtdLuz, statusLuminosidade, alerta, momentoCaptura, fkDadosSensor_Sensor) VALUES
 	(35.50, 'Satisfatória', 'Não', '2024-09-08 08:00:00', 10000),
@@ -147,4 +154,5 @@ INSERT INTO dadosSensor (qtdLuz, statusLuminosidade, alerta, momentoCaptura, fkD
 	(30.90, 'Satisfatória', 'Não', '2024-09-25 14:00:00', 10004),
 	(12.10, 'Baixa', 'Não', '2024-09-26 08:00:00', 10005),
 	(8.70, 'Baixa', 'Não', '2024-09-26 18:00:00', 10006),
-	(50.25, 'Satisfatória', 'Não', '2024-10-04 12:00:00', 10007);
+	(50.25, 'Satisfatória', 'Não', '2024-10-04 12:00:00', 10007),
+    (16.00, 'Baixa', 'Sim', '2024-10-05 18:00:00', 10008);
