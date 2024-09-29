b2bde75bd0461a5a10215a510af54a6b2eb50212
oizaeljr <izael.oliveirajr@sptech.school>
2024-09-28 22:07:12 -0300

    Adicionei a versão tela cheia

--- Cadastro - Login/VersaoMaior/funcionalidades.js
+++ Cadastro - Login/VersaoMaior/funcionalidades.js
@@ -1,11 +1,5 @@
-function AbrirLogin() {
-    const urlTela = 'login.html'; // Endereço da página login do html
-    window.close(); // Fecha a página aberta (cadastro)
-    window.open(urlTela); // Abre a página login
-}
-
 function AbrirCadastro() {
-    const urlTela = 'cadastro.html'; // Endereço da página cadastro do html
+    const urlTela = 'cadastronovo.html'; // Endereço da página cadastro do html
     window.close(); // Fecha a página aberta (login)
     window.open(urlTela); // Abre a página cadastro
 }
@@ -13,9 +7,9 @@ function AbrirCadastro() {
 function VisualizarSenha() {
     if (inp_senha2.type == 'password') { // Se o tipo da input for password 
         inp_senha2.type = 'text'; // Transforma em text, ou seja, deixa a senha visível
-        img_olho3.src = 'Imagens/olho_fechado.png'; // Abre a imagem do olho fechado
+        img_olho3.src = '../Imagens/olho_fechado.png'; // Abre a imagem do olho fechado
     } else {
         inp_senha2.type = 'password'; // Caso a senha já esteja como text (visível), volta a ser password e esconde a senha
-        img_olho3.src = 'Imagens/olho_aberto.png'; // Abre a imagem do olho aberto
+        img_olho3.src = '../Imagens/olho_aberto.png'; // Abre a imagem do olho aberto
     }
-}
+} 
