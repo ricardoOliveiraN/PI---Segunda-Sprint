56082c22e9a4b8c686196cbb4feebecade1f421c
Joao gabriel <joao.nogueira@sptech.school>
2024-10-17 17:08:01 -0300

    atualizei o codigo arduino

--- Arduino/codigo-ldr/codigo-ldr.ino
+++ Arduino/codigo-ldr/codigo-ldr.ino
@@ -1,25 +1,19 @@
-const int PINO_SENSOR_LDR = A3; // declara a entrada analógica
-int valorLuminosidade; // declara uma variável
-
+int ldr_pin = A5;
+int ldr_read = 0;
+float vin = 5.00;
+float valor_ADC = 0.00488758, r_ohms = 10000;
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
-// configuração de iniciação e comunicação entre arduino e computador
 
 void loop() {
-  valorLuminosidade = analogRead(PINO_SENSOR_LDR);
-  // os dados coletados pela porta analogica serão armazenados na variável
-  // definindo labels/variáveis e valores que serão mostrados na tela para melhorar o entendimento do gráfico
-  Serial.print("MiniVoltsMax:");
-  Serial.print(900);
-  Serial.print(" ");
-  Serial.print("Luminosidade:");
-  Serial.print(valorLuminosidade);
-  Serial.print(" ");
-  Serial.print("MiniVoltsMin:");
-  Serial.println(400);
+        ldr_read = analogRead(ldr_pin);
+        float volt = valor_ADC * ldr_read;
+        float res_ldr = (r_ohms * (vin - volt)) / volt;
+        float lumens = 500/(res_ldr/1000);
 
-  delay(2000);
-  // configura para atualizar os dados a cada 2 segundos
+
+        Serial.print(lux);
+    delay(2000);
 }
